2e3be06f64396bc94a00de96a482baaf8c9aab17
mbjn <mbjn@itu.dk>
2022-10-11 13:22:34 +0200

    Added conjured, fixed backstage pass, cleaned code, added @lauritsbrok tests.

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 namespace GildedRose
 {
     public class Program {
-        IList<AbstractItem> Items;
+        public IList<NormalItem> Items;
         public static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
             var app = new Program() {
-                Items = new List<AbstractItem> {
+                Items = new List<NormalItem> {
                     new NormalItem { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
                     new Cheese { Name = "Aged Brie", SellIn = 2, Quality = 0 },
                     new NormalItem { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
@@ -24,18 +24,15 @@ namespace GildedRose
                 }
             };
 
-            for (var i = 0; i < 31; i++)
-            {
+            for (var i = 0; i < 31; i++) {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < app.Items.Count; j++)
-                {
-                    Console.WriteLine(app.Items[j].Name + ", " + app.Items[j].SellIn + ", " + app.Items[j].Quality);
+                foreach (var item in app.Items) {
+                    Console.WriteLine(item.Name + ", " + item.SellIn + ", " + item.Quality);
                 }
                 Console.WriteLine("");
                 app.UpdateQuality();
             }
-
         }
 
         public void UpdateQuality() {
@@ -43,74 +40,80 @@ namespace GildedRose
                 item.UpdateQuality();
             }
         }
+    }
+}
 
-        public class Item
-        {
-            public string Name { get; set; }
+public class Item
+{
+    public string Name { get; set; }
 
-            public int SellIn { get; set; }
+    public int SellIn { get; set; }
 
-            public int Quality { get; set; }
+    public int Quality { get; set; }
+}
+public class NormalItem : Item {
+    protected int QualityDecrease = -1;
+    public virtual void UpdateQuality(bool SellInAfter = false) { //For some reason, all items except backstage passes, have their 'sellIn' value decreased before calculating their next quality.
+        if (SellInAfter == false) SellIn--;
+
+        DetermineQualityDecrease();
+        Quality += QualityDecrease;
+        if (Quality < 0) Quality = 0;
+        if (Quality > 50) Quality = 50;
+
+        if (SellInAfter) SellIn--;
+    }
+
+    public virtual void DetermineQualityDecrease() {
+        if (SellIn < 0) {
+            QualityDecrease = -2;
         }
+    }
+}
 
-        public abstract class AbstractItem : Item {
-            protected int QualityDecrease = -1;
-            public virtual void UpdateQuality() {
-                SellIn--;
-                DetermineQualityDecrease();
-                Quality += QualityDecrease;
-
-                if (Quality < 0) Quality = 0;
-            }
-
-            public virtual void DetermineQualityDecrease() {
-                if (SellIn < 0) {
-                    QualityDecrease = -2;
-                }
-            }
+public class Cheese : NormalItem {
+    public override void DetermineQualityDecrease() {
+        if (SellIn >= 0) {
+            QualityDecrease = 1;
         }
-
-
-        public class NormalItem : AbstractItem {
-            
+        else {
+            QualityDecrease = 2;
         }
-        public class Cheese : AbstractItem {
-            public override void DetermineQualityDecrease() {
-                if (SellIn >= 0) {
-                    QualityDecrease = 1;
-                }
-                else {
-                    QualityDecrease = 2;
-                }
-                
-            }
-        }
-        public class Legendary : AbstractItem {
-            public override void UpdateQuality() {
-            }
-        }
-        public class BackstagePass : AbstractItem {
-            public override void UpdateQuality() {
-                base.UpdateQuality();
+    }
+}
+public class Legendary : NormalItem {
+    public override void UpdateQuality(bool SellInAfter = false) {
+    }
+}
+public class BackstagePass : NormalItem {
+    public override void UpdateQuality(bool SellInAfter = false) {
+        base.UpdateQuality(true);
 
-                if (SellIn < 0 ) {
-                    Quality = 0;
-                }
-            }
-            public override void DetermineQualityDecrease() {
-                if (SellIn < 0) {
-                    QualityDecrease = 0;
-                }
-                else if (SellIn <= 5) {
-                    QualityDecrease = 3;
-                }
-                else if (SellIn <= 10) {
-                    QualityDecrease = 2;
-                }
-                else {
-                    QualityDecrease = 1;
-                }
-            }
+        if (SellIn < 0 ) {
+            Quality = 0;
+        }
+    }
+    public override void DetermineQualityDecrease() {
+        if (SellIn < 0) {
+            QualityDecrease = 0;
+        }
+        else if (SellIn <= 5) {
+            QualityDecrease = 3;
+        }
+        else if (SellIn <= 10) {
+            QualityDecrease = 2;
+        }
+        else {
+            QualityDecrease = 1;
+        }
+    }
+}
+
+public class Conjured : NormalItem {
+    public override void DetermineQualityDecrease() {
+        QualityDecrease = -2;
+        if (SellIn < 0) {
+            QualityDecrease = -4;
         }
     }
 }
